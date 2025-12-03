87cb2cd8c436eca582063c15eef91379fb40eecf
CapitaineToinon <capitainetoinon@proton.me>
2025-12-03 04:25:16 +0100

    grouped solutions for quest 4

--- quest04/src/main.rs
+++ quest04/src/main.rs
@@ -16,37 +16,14 @@ fn parse(input: &String) -> Gears {
         .collect()
 }
 
-fn part_1(gears: &Gears) -> i64 {
+fn left_to_right(gears: &Gears, rotations: f64) -> i64 {
     gears
         .windows(2)
-        .fold(2025.0, |acc, w| match w {
-            [(a, _), (b, _)] => acc * (a / b),
-            _ => panic!("invalid window"),
-        })
-        .floor() as i64
-}
-
-fn part_2(gears: &Gears) -> i64 {
-    // same as part 1 but need to ceil, traverse the windows
-    // in reverse direction and divide instead of mutliply the ratios
-    gears
-        .windows(2)
-        .rev()
-        .fold(10000000000000.0, |acc, w| match w {
-            [(a, _), (b, _)] => acc / (a / b),
-            _ => panic!("invalid window"),
-        })
-        .ceil() as i64
-}
-
-fn part_3(gears: &Gears) -> i64 {
-    gears
-        .windows(2)
-        .fold(100.0, |acc, w| match w {
+        .fold(rotations, |acc, w| match w {
             [(a1, a2), (b, _)] => {
                 acc * match (a1, a2) {
-                    (x, None) => x / b,
                     (_, Some(x)) => x / b,
+                    (x, None) => x / b,
                 }
             }
             _ => panic!("invalid window"),
@@ -54,14 +31,30 @@ fn part_3(gears: &Gears) -> i64 {
         .floor() as i64
 }
 
+fn right_to_left(gears: &Gears, rotations: f64) -> i64 {
+    // same as left_to_right but need to ceil, traverse the windows
+    // in reverse direction and divide instead of mutliply the ratios
+    gears
+        .windows(2)
+        .rev()
+        .fold(rotations, |acc, w| match w {
+            // technically we should check for mounted gears
+            // also when going from right to left but it's
+            // not required for part_2
+            [(a, _), (b, _)] => acc / (a / b),
+            _ => panic!("invalid window"),
+        })
+        .ceil() as i64
+}
+
 fn main() {
     let args = parse_args();
     let gears = parse(&args.input);
 
     let solution = match args.part {
-        Part::Part1 => part_1(&gears),
-        Part::Part2 => part_2(&gears),
-        Part::Part3 => part_3(&gears),
+        Part::Part1 => left_to_right(&gears, 2025.0),
+        Part::Part2 => right_to_left(&gears, 10000000000000.0),
+        Part::Part3 => left_to_right(&gears, 100.0),
     };
 
     println!("{}", solution);
