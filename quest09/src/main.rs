96f23a04aacf7e45f27c10e36b8134c2759dcd51
CapitaineToinon <capitainetoinon@proton.me>
2025-12-06 22:43:34 +0100

    improved quest 9 from ~3 minutes to ~5 seconds
    it's not much but it's honest work

--- quest09/src/main.rs
+++ quest09/src/main.rs
@@ -1,8 +1,76 @@
+use common::{Part, parse_args};
 use std::collections::{HashMap, HashSet};
 
-use common::{Part, parse_args};
+type Dna = (usize, Vec<u128>);
+type Person = (usize, Dna);
 
-type Person = (usize, String);
+fn map_char(c: u8) -> u128 {
+    match c {
+        c if c == 'A' as u8 => 0b0001,
+        c if c == 'C' as u8 => 0b0010,
+        c if c == 'T' as u8 => 0b0100,
+        c if c == 'G' as u8 => 0b1000,
+        _ => panic!("invalid dna sequence"),
+    }
+}
+
+fn get_similarity((len1, dna1): &Dna, (len2, dna2): &Dna, (len3, dna3): &Dna) -> Option<usize> {
+    // bail if the dna aren't all the same size
+    if len1 != len2 || len1 != len3 {
+        return None;
+    }
+
+    let mut score_a = 0;
+    let mut score_b = 0;
+
+    for (c, (p1, p2)) in dna1.iter().zip(dna2.iter().zip(dna3)) {
+        // bail if not related
+        if (c ^ (c & p1)) & (c ^ (c & p2)) != 0 {
+            return None;
+        }
+
+        // start computing the similary score
+        for i in 0..32 {
+            let s = i * 4;
+            let m = 0b1111;
+            let c = (c >> s) & m;
+            let a = (p1 >> s) & m;
+            let b = (p2 >> s) & m;
+
+            if c == 0 || a == 0 || b == 0 {
+                break;
+            }
+
+            if a == c {
+                score_a += 1;
+            }
+
+            if b == c {
+                score_b += 1;
+            }
+        }
+    }
+
+    Some(score_a * score_b)
+}
+
+fn parse_dna(dna: &String) -> Dna {
+    let len = dna.len();
+
+    let dna = dna
+        .bytes()
+        .collect::<Vec<u8>>()
+        .chunks(32) // sizeof u128
+        .map(|bytes| {
+            bytes
+                .iter()
+                .enumerate()
+                .fold(0u128, |acc, (i, &byte)| acc + (map_char(byte) << (4 * i)))
+        })
+        .collect::<Vec<u128>>();
+
+    (len, dna)
+}
 
 fn parse(input: &String) -> Vec<Person> {
     input
@@ -10,109 +78,66 @@ fn parse(input: &String) -> Vec<Person> {
         .split("\n")
         .map(|line| line.to_string())
         .map(|line| match line.split_once(":") {
-            Some((id, dna)) => (id.parse().expect("failed to parse id"), dna.to_string()),
+            Some((id, dna)) => (
+                id.parse().expect("failed to parse id"),
+                parse_dna(&dna.to_string()),
+            ),
             _ => panic!("invalid input"),
         })
-        .collect::<Vec<(usize, String)>>()
+        .collect::<Vec<Person>>()
 }
 
-fn find_parents((id, dna): &Person, everyone: &Vec<Person>) -> Option<Vec<Person>> {
-    let candidates = everyone
-        .iter()
-        .filter(|(other_id, _)| other_id != id)
-        .cloned()
-        .collect::<Vec<Person>>();
+fn find_parents((id, dna): &Person, everyone: &Vec<Person>) -> Option<(usize, usize, usize)> {
+    for a in 0..everyone.len() {
+        for b in a..everyone.len() {
+            let (id_a, dna_a) = &everyone[a];
+            let (id_b, dna_b) = &everyone[b];
 
-    let combinations = candidates
-        .iter()
-        .enumerate()
-        .flat_map(|(i, a)| {
-            candidates[i..candidates.len()]
-                .iter()
-                .map(|b| (a.clone(), b.clone()))
-        })
-        .collect::<Vec<(Person, Person)>>();
+            if id == id_a || id_b == id {
+                continue;
+            }
 
-    'next_candidate: for ((id_a, dna_a), (id_b, dna_b)) in combinations {
-        let chars = dna_a
-            .chars()
-            .zip(dna_b.chars())
-            .collect::<Vec<(char, char)>>();
-
-        for (c, (char_a, char_b)) in dna.chars().zip(chars) {
-            if c != char_a && c != char_b {
-                continue 'next_candidate;
+            if let Some(score) = get_similarity(&dna, &dna_a, &dna_b) {
+                return Some((*id_a, *id_b, score));
             }
         }
-
-        // found the child
-        return Some(Vec::from([(id_a, dna_a), (id_b, dna_b)]));
     }
 
     None
 }
 
-fn find_child(people: &Vec<Person>) -> Option<(Person, Vec<Person>)> {
+fn find_first_relation(people: &Vec<Person>) -> Option<(usize, usize, usize, usize)> {
     for child in people {
-        if let Some(parents) = find_parents(&child, &people) {
-            return Some((child.clone(), parents));
+        if let Some((id_a, id_b, score)) = find_parents(&child, &people) {
+            let &(id, _) = child;
+            return Some((id, id_a, id_b, score));
         }
     }
 
     None
-}
-
-fn compute_similarity((_, dna): (usize, String), parents: &Vec<(usize, String)>) -> usize {
-    parents
-        .iter()
-        .map(|(_, other_dna)| {
-            dna.chars()
-                .zip(other_dna.chars())
-                .filter(|(c1, c2)| c1 == c2)
-                .count()
-        })
-        .fold(1, |acc, count| acc * count)
 }
 
 fn compute_similarities(everyone: &Vec<Person>) -> usize {
-    dbg!(
-        everyone
-            .iter()
-            .map(|child| match find_parents(child, &everyone) {
-                Some(parents) => Some((child.clone(), parents)),
-                _ => None,
-            })
-            .collect::<Vec<Option<(Person, Vec<Person>)>>>()
-    );
+    let mut total = 0;
 
-    everyone
-        .iter()
-        .map(|child| match find_parents(child, &everyone) {
-            Some(parents) => Some((child.clone(), parents)),
-            _ => None,
-        })
-        .filter_map(|p| p)
-        .collect::<Vec<(Person, Vec<Person>)>>()
-        .iter()
-        .map(|(child, parents)| compute_similarity(child.clone(), &parents))
-        .sum()
+    for child in everyone {
+        if let Some((_, _, score)) = find_parents(child, &everyone) {
+            total += score;
+        }
+    }
+
+    total
 }
 
 fn build_graph(everyone: &Vec<Person>) -> HashMap<usize, HashSet<usize>> {
     let mut relations: HashMap<usize, HashSet<usize>> = HashMap::new();
 
     for person in everyone {
-        if let Some(p) = find_parents(person, &everyone) {
+        if let Some((id_a, id_b, _)) = find_parents(person, &everyone) {
             let (id, _) = person;
-
-            relations
-                .entry(*id)
-                .or_default()
-                .extend(p.iter().map(|(id, _)| id));
-
-            for (pid, _) in p {
-                relations.entry(pid).or_default().extend([id]);
-            }
+            relations.entry(*id).or_default().extend([id_a, id_b]);
+            relations.entry(id_a).or_default().extend([id]);
+            relations.entry(id_b).or_default().extend([id]);
         }
     }
 
@@ -157,8 +182,9 @@ fn main() {
 
     let solution = match args.part {
         Part::Part1 => {
-            let (child, parents) = find_child(&people).expect("failed to find a valid child");
-            compute_similarity(child, &parents)
+            let (_, _, _, score) =
+                find_first_relation(&people).expect("failed to find a valid child");
+            score
         }
         Part::Part2 => compute_similarities(&people),
         Part::Part3 => {
